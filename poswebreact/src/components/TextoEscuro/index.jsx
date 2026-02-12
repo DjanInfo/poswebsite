7a5e09fc0e5be1cf3a4c53c4907e43c0cee84016
Isabela Damasceno <161511929+isabeladdamasceno@users.noreply.github.com>
2026-02-11 22:12:36 -0300

    Adicionando os componentes texto e titulo escuro na feature
    Adicionando os componentes texto e titulo escuro na feature

--- poswebreact/src/components/TextoEscuro/index.jsx
+++ poswebreact/src/components/TextoEscuro/index.jsx
@@ -1,4 +1,7 @@
-export default function Texto_Escuro({ children }) {
-  return ( <p className="mt-8 text-lg lg:text-xl leading-relaxed text-gray-700">{children}</p>
-  )
+export default function Texto_Escuro({ children, className = "" }) {
+  return (
+    <p className={`text-lg lg:text-xl leading-relaxed text-gray-700 ${className}`}>
+      {children}
+    </p>
+  );
 }
