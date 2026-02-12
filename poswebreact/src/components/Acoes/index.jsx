79dc541c5b84e84d054c24fb3118480eebaad8f0
João Vitor <vitorrlemos2004@gmail.com>
2026-02-12 11:45:51 -0300

    Adiciona componentes, ícones e ajustes no Hero - João

---
+++ poswebreact/src/components/Acoes/index.jsx
@@ -0,0 +1,17 @@
+import edicao from "../../assets/icons/edicao.png";
+import excluir from "../../assets/icons/excluir.png";
+import mostrar from "../../assets/icons/mostrar.png";
+
+export default function Acoes() {
+  return (
+    <div className="flex items-center justify-center gap-2">
+      <img src={mostrar} alt="Mostrar" title="Mostrar" 
+      className="w-4 h-4 cursor-pointer transition-transform duration-150 hover:scale-110 hover:opacity-80"/>
+      <img src={edicao} alt="Editar" title="Editar" 
+      className="w-4 h-4 cursor-pointer transition-transform duration-150 hover:scale-110 hover:opacity-80"/>
+      <img src={excluir} alt="Excluir" title="Excluir" 
+      className="w-4 h-4 cursor-pointer transition-transform duration-150 hover:scale-110 hover:opacity-80"/>
+    </div>
+  );
+}
+
