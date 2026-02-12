6b31b4162bff468c91f75290706b789328103fae
Karolina Disigant <anadisigant@gmail.com>
2026-02-12 11:37:38 -0300

    criando topo do modelo

--- poswebreact/src/components/Modelo/index.jsx
+++ poswebreact/src/components/Modelo/index.jsx
@@ -1,7 +1,44 @@
-import Texto_Escuro from "../TextoEscuro";
+import { useState } from "react";
+import TituloTopo from "../TituloTopo";
+import ControleLista from "../ControleLista";
 
-export default function Modelo() {
+export default function Modelo({ children }) {
+  const [pesquisa, setPesquisa] = useState("");
+
   return (
-    <Texto_Escuro>Modelo</Texto_Escuro>
+    <div className="w-full bg-gray-50 min-h-screen">
+      {/* Container central da página */}
+      <div className="
+        max-w-7xl
+        mx-auto
+        px-6
+        py-8
+        flex
+        flex-col
+        gap-6
+      ">
+        
+        {/* Header da página */}
+        <div className="flex justify-between items-start">
+          <TituloTopo
+            titulo="Lista de Alunos"
+            subtitulo="Gerencie e visualize todos os alunos matriculados"
+          />
+
+          <ControleLista
+            pesquisa={pesquisa}
+            onPesquisa={(e) => setPesquisa(e.target.value)}
+            onFiltro={() => console.log("filtro")}
+            onExportar={() => console.log("exportar")}
+            placeholderPesquisa="Buscar aluno..."
+          />
+        </div>
+
+        <div className="flex flex-col gap-6">
+          {children}
+        </div>
+
+      </div>
+    </div>
   );
 }
