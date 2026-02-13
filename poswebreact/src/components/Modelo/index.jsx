e0f9a28f491d31670b39e61f9bca072a6ac6b8ad
Isabela Damasceno <161511929+isabeladdamasceno@users.noreply.github.com>
2026-02-13 10:47:27 -0300

    Componente Titulo Tabela
    Componente Titulo Tabela

--- poswebreact/src/components/Modelo/index.jsx
+++ poswebreact/src/components/Modelo/index.jsx
@@ -1,7 +1,23 @@
+import { useState } from "react";
 import Texto_Escuro from "../TextoEscuro";
+import TituloTabela from "../TituloTabela";
 
 export default function Modelo() {
+  const [paginaAtual, setPaginaAtual] = useState(1);
+
   return (
-    <Texto_Escuro>Modelo</Texto_Escuro>
+    <>
+      <Texto_Escuro>Modelo</Texto_Escuro>
+
+      <TituloTabela
+        titulo="Alunos Matriculados"
+        paginaAtual={paginaAtual}
+        totalPaginas={3}
+        totalRegistros={1247}
+        inicio={1}
+        fim={10}
+        onPaginaChange={setPaginaAtual}
+      />
+    </>
   );
 }
