4b8197b1cca537b8247f459f69ef4dac7f092829
DjanInfo <djan.info@gmail.com>
2026-02-13 11:11:53 -0300

    Atualizacao modelo2

--- poswebreact/src/components/Modelo/index.jsx
+++ poswebreact/src/components/Modelo/index.jsx
@@ -1,23 +1,7 @@
-import { useState } from "react";
 import Texto_Escuro from "../TextoEscuro";
-import TituloTabela from "../TituloTabela";
 
 export default function Modelo() {
-  const [paginaAtual, setPaginaAtual] = useState(1);
-
   return (
-    <>
-      <Texto_Escuro>Modelo</Texto_Escuro>
-
-      <TituloTabela
-        titulo="Alunos Matriculados"
-        paginaAtual={paginaAtual}
-        totalPaginas={3}
-        totalRegistros={1247}
-        inicio={1}
-        fim={10}
-        onPaginaChange={setPaginaAtual}
-      />
-    </>
+    <Texto_Escuro>Modelo</Texto_Escuro>
   );
 }
