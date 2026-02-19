a3331ae9cf245f212267d558714c7903dfbd4a54
viquituor <vitoralmeide@gmail.com>
2026-02-19 10:59:38 -0300

    pagina de ouvidoria

---
+++ poswebreact/src/pages/ouvidoria/ouvidoria.columns.js
@@ -0,0 +1,5 @@
+export const colunasOuvidoria = [
+    {titulo: "ID", campo: "id", tipo: "texto"},
+    {titulo:"AUTOR", campo: "autor", tipo: "texto"},
+    {titulo: "TEXTO", campo: "texto", tipo: "texto"}
+];
