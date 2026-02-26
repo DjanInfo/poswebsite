43e9ab6743b8e5000431db38d6fa4d504620b2ae
HiagoAlvesdasilva <hiagoalvesdasilva11@gmail.com>
2026-02-26 16:16:30 -0300

    Ajustes gerais

--- poswebreact/src/App.jsx
+++ poswebreact/src/App.jsx
@@ -1,4 +1,5 @@
 import { Routes, Route } from "react-router-dom";
+
 import Header from "./components/Header";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
@@ -7,32 +8,39 @@ import ProcessosSeletivos from "./components/ProcessosSeletivos";
 import Noticias from "./components/Noticias";
 import Numeros from "./components/Numeros";
 import Footer from "./components/Footer";
-import TituloTabela from "./components/TituloTabela";
+
 import Alunos from "./pages/alunos/Aluno";
 import AlunoCreate from "./pages/alunos/alunoCreate";
 import AlunoShow from "./pages/alunos/alunoShow";
 import AlunoEdit from "./pages/alunos/alunoEdit";
+
 import ModeloPage from "./pages/modelo/AlunoPage";
 import ModeloCreatePage from "./pages/modelo/AlunoCreatePage";
 import ModeloShowPage from "./pages/modelo/AlunoShowPage";
 import ModeloEditPage from "./pages/modelo/AlunoEditPage";
+
 import Ouvidoria from "./pages/ouvidoria/OuvidoriaPage";
+import OuvidoriaShowPage from "./pages/ouvidoria/OuvidoriaShowPage";
+import OuvidoriaEditPage from "./pages/ouvidoria/OuvidoriaEditePage";
+import OuvidoriaCreatePage from "./pages/ouvidoria/OuvidoriaCreatepage";
+
 import NoticiaPage from "./pages/noticias/NoticiaPage";
 import NoticiaShowPage from "./pages/noticias/NoticiaShowPage";
 import NoticiaCreatePage from "./pages/noticias/NoticiaCreatePage";
 import NoticiaEditPage from "./pages/noticias/NoticiaEditPage";
+
 import DocentePage from "./pages/Docente/Docentepage";
-import InscricaoPage from "./pages/inscricoes/InscricoesPage";
-import OuvidoriaShowPage from "./pages/ouvidoria/OuvidoriaShowPage";
-import OuvidoriaEditPage from "./pages/ouvidoria/OuvidoriaEditePage";
-import OuvidoriaCreatePage from "./pages/ouvidoria/OuvidoriaCreatepage";
+
 import EditalPage from "./pages/editais/EditalPage";
 import EditalCreatePage from "./pages/editais/EditalCreatePage";
 import EditalShowPage from "./pages/editais/EditalShowPage";
 import EditalEditPage from "./pages/editais/EditalEditPage";
-import InscricoesEditPage from "./pages/inscricoes/InscricoesEditPage";
+
+import InscricaosPage from "./pages/inscricoes/InscricoesPage";
 import InscricoesShowPage from "./pages/inscricoes/InscricoesShowPage";
+import InscricoesEditPage from "./pages/inscricoes/InscricoesEditPage";
 import InscricaoCreatePage from "./pages/inscricoes/InscricaoCreatePage";
+
 
 function Home() {
   return (
@@ -53,36 +61,49 @@ export default function App() {
       <Navbar />
 
       <Routes>
+
+        {/* HOME */}
         <Route path="/" element={<Home />} />
+
+        {/* MODELO */}
         <Route path="/modelo" element={<ModeloPage />} />
         <Route path="/modelo/novo" element={<ModeloCreatePage />} />
         <Route path="/modelo/:matricula" element={<ModeloShowPage />} />
         <Route path="/modelo/:matricula/editar" element={<ModeloEditPage />} />
+
+        {/* OUVIDORIA */}
         <Route path="/ouvidorias" element={<Ouvidoria />} />
         <Route path="/ouvidorias/novo" element={<OuvidoriaCreatePage />} />
         <Route path="/ouvidorias/:id" element={<OuvidoriaShowPage />} />
         <Route path="/ouvidorias/:id/editar" element={<OuvidoriaEditPage />} />
-        <Route path="/aluno" element={<Alunos />} />
 
+        {/* ALUNOS */}
+        <Route path="/aluno" element={<Alunos />} />
         <Route path="/aluno/novo" element={<AlunoCreate />} />
         <Route path="/aluno/:matricula" element={<AlunoShow />} />
         <Route path="/aluno/:matricula/editar" element={<AlunoEdit />} />
-        <Route path="/ouvidoria" element={<Ouvidoria />} />
+
+        {/* NOTICIAS */}
         <Route path="/noticias" element={<NoticiaPage />} />
         <Route path="/noticias/novo" element={<NoticiaCreatePage />} />
         <Route path="/noticias/:id" element={<NoticiaShowPage />} />
         <Route path="/noticias/:id/editar" element={<NoticiaEditPage />} />
+
+        {/* DOCENTES */}
         <Route path="/docentes" element={<DocentePage />} />
 
-        <Route path="/inscricoes" element={<InscricaoPage />} />
+        {/* INSCRIÇÕES */}
+        <Route path="/inscricoes" element={<InscricaosPage />} />
         <Route path="/inscricoes/criar" element={<InscricaoCreatePage />} />
         <Route path="/inscricoes/:id" element={<InscricoesShowPage />} />
         <Route path="/inscricoes/:id/editar" element={<InscricoesEditPage />} />
 
+        {/* EDITAIS */}
         <Route path="/editais" element={<EditalPage />} />
         <Route path="/editais/novo" element={<EditalCreatePage />} />
         <Route path="/editais/:id" element={<EditalShowPage />} />
         <Route path="/editais/:id/editar" element={<EditalEditPage />} />
+
       </Routes>
 
       <Footer />
